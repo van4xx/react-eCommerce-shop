387d67a7f46e200baddf62b527316d6b4fc794ec
Van4xx <van4xx@ya.ru>
2024-03-10 12:08:40 +0300

    Добавить товар в корзину

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
 
+const showOrders = (props) => {
+  return (
+    <div>
+        {props.orders.map( el => (
+          <Order key={el.id} item={el} />
+        ))}
+    </div>
+  )
+}
 
-export default function Header() {
+const showNothing = () => {
+  return(
+    <div className='empty'>
+      <h2>Товаров нет</h2>
+    </div>
+  )
+}
+
+export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false)
 
   return (
@@ -18,6 +36,8 @@ export default function Header() {
 
             {cartOpen && (
               <div className='shopCart'>
+                {props.orders.length > 0 ?
+                  showOrders(props) : showNothing()}
               </div>
             )}
         </div>
